abf6680dc4697ead487f7dde2080cdf64b4cbad1
jmcimula <jmkacim@gmail.com>
2016-08-10 03:10:25 +0200

    List of regions

---
+++ R/variety.R
@@ -0,0 +1,18 @@
+#' Focusing on providing high quality music streaming content
+#'
+#'@details
+#'
+#'List of all regions of the world
+#'
+#'@export
+
+ethno_music_type <- function(){
+
+variety <- c( "African", "Asian", "Australian", "Balkan", "Caribbean", "East%20European",
+              "Gypsy", "India", "Jewish", "Latin%20American", "Mediterranean", "Melanesian",
+              "Micronesian", "Middle%20Eastern", "New%20Guinean", "North%20African", "North%20American",
+              "Polynesian", "Scandinavian", "West%20European"
+	        )
+return(variety)
+}
+
